9c551f78f7f8e817c02f890a7e24940d3a39cce6
ohbumjun <beomjun0638@hanmail.net>
2022-08-13 21:24:39 +0900

    refactor(Engine) Create DestroyExistingParticle Function In Particle Component

--- Client3D/Include/DeathDoor/Component/TriggerFireLamp.cpp
+++ Client3D/Include/DeathDoor/Component/TriggerFireLamp.cpp
@@ -1,6 +1,8 @@
 #include "TriggerFireLamp.h"
 #include "ArrowCollisionFireCollider.h"
 #include "Component/LightComponent.h"
+#include "Scene/SceneManager.h"
+#include "Scene/Scene.h"
 
 // ArrowCollisionFireCollider 들을 관리하는 Object Data Component
 CTriggerFireLamp::CTriggerFireLamp() :
@@ -22,6 +24,8 @@ void CTriggerFireLamp::IncActiveCount()
 		m_CurrentActive = true;
 
 		// 여기에서 SceneMode 에 다 끝났다고 알려주기 
+		// CPU_SET_INFORMATION_TYPE
+
 	}
 }
 
